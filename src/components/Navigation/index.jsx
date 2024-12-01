616247180e1b159ec85616c8c7501b8b62433fe6
Mehmet Ardic <ardicmehmet96@gmail.com>
2024-12-01 22:30:33 +0300

    deleted some codes and optimized

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,37 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { NavLink, useLocation, useSearchParams } from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import style from "./navigation.module.css";
-/* ( / ve /movies e yönlendirme olucak burda ) */
 
 const buildLinkClass = ({ isActive }) => {
   return clsx(style.link, isActive && style.active);
 };
 const Navigation = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const location = useLocation();
-
   return (
     <header>
       <nav>
-        <NavLink
-          to="/"
-          state={{
-            from: location.pathname,
-            query: searchParams.get("query") || "",
-          }}
-          className={buildLinkClass}
-        >
+        <NavLink to="/" className={buildLinkClass}>
           Home
         </NavLink>
-        <NavLink
-          to="/movies"
-          state={{
-            from: location.pathname,
-            query: searchParams.get("query") || "",
-          }}
-          className={buildLinkClass}
-        >
+        <NavLink to="/movies" className={buildLinkClass}>
           Movies
         </NavLink>
       </nav>
